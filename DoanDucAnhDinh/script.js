7b9acac20a606bc2ce0b971fb53d2986493d4f29
S357416 <113960511+S357416@users.noreply.github.com>
2023-04-11 21:47:22 +0930

    Add files via upload

---
+++ DoanDucAnhDinh/script.js
@@ -0,0 +1,23 @@
+function about_us_link(){
+    window.open("about_us.html", "_self")
+   }
+
+function governance_link() {
+    window.open("governance.html", "_self")
+}
+
+function policies_link() {
+    window.open("policies.html", "_self")
+}
+
+function how_to_join_link() {
+    window.open("how_to_join.html", "_self")
+}
+
+function testing_link() {
+    window.open("testing.html", "_self")
+}
+
+function contact_us_link() {
+    window.open("contact_us.html", "_self")
+}
