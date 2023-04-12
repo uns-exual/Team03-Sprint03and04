9f39bcef904e5b9c5403a774c2af8cc6a6c04163
s350764 <127164618+s350764@users.noreply.github.com>
2023-04-12 22:26:12 +0930

    Add files via upload

---
+++ VietLeHoang/scripts.js
@@ -0,0 +1,12 @@
+function Policy_link(){
+    window.open("read_more.html", "_self")
+   }
+function Testing_link(){
+    window.open("read_more.html", "_self")
+   }
+function Joining_link(){
+    window.open("read_more.html", "_self")
+   }
+function Contact_link(){
+    window.open("read_more.html", "_self")
+   }
